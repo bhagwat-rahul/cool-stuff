3d4a3a24b91edf3997fd3df9fd880f25702d7d16
Rahul Bhagwat <rahulmbme@gmail.com>
2024-12-01 21:55:14 +0530

    Update solution.c

--- Advent-Of-Code-2024/Day-1/solution.c
+++ Advent-Of-Code-2024/Day-1/solution.c
@@ -1,15 +1,31 @@
 # include <stdio.h>
 
 
-// Create helper func to find smallest int
-void sortList(int *list, int len)
+// create helper func to find smallest int
+void sortList(int *list, int len, int *sortedList)
 {
-    // Sort from low to high
+    // sort from low to high into a new array
 }
 
-void listCompare(int *list1, int *list2, int listLen)
+int listCompare(int *list1, int *list2, int listLen)
 {
+    int result = 0;
     // compare indexes and subtract larger from smaller
+    for (int i = 0; i <listLen; i++)
+        {
+            if (list1[i]<list2[i])
+                {
+                    result+=list2[i]-list1[i];
+                }
+            else if (list1[i]>list2[i])
+                {
+                    result+=list1[i]-list2[i];
+                }
+            else {
+                    result+=0;
+                }
+        }
+    return result;
 }
 
 int main()
@@ -17,7 +33,11 @@ int main()
     // initialise both arrays
     int a[999];
     int b[999];
+    int aSorted[999];
+    int bSorted[999];
+    int result = 0;
 
-    printf("End of Program");
+    result = listCompare(aSorted, bSorted, 999);
+    printf("%d", result);
     return 0;
 }
