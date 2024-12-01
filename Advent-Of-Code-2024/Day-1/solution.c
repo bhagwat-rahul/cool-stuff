ae55696e656245fc9864106858227a5901045a27
Rahul Bhagwat <rahulmbme@gmail.com>
2024-12-01 21:23:14 +0530

    init Advent of code 2024

---
+++ Advent-Of-Code-2024/Day-1/solution.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+
+// Create helper func to find smallest int
+void sortList(int *list, int len)
+{
+    // Sort from low to high
+}
+
+void listCompare(int *list1, int *list2, int listLen)
+{
+    // compare indexes and subtract larger from smaller
+}
+
+int main()
+{
+    // initialise both arrays
+    int a[999];
+    int b[999];
+
+    printf("End of Program");
+    return 0;
+}
