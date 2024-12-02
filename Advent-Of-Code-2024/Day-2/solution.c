54e6c246dd3ebda376638353f338b4291430951c
Rahul Bhagwat <rahulmbme@gmail.com>
2024-12-02 16:24:50 +0530

    check if greater or smaller

--- Advent-Of-Code-2024/Day-2/solution.c
+++ Advent-Of-Code-2024/Day-2/solution.c
@@ -1,6 +1,6 @@
 # include <stdio.h>
 
-void scanFilesCreateReports(int reportData[30][300], int levelLen, int reportLen)
+void scanFilesCreateReports(int reportData[][300], int levelLen, int reportLen)
 {
     FILE *input = fopen("input.txt", "r");
     if (input == NULL) {
@@ -12,21 +12,40 @@ void scanFilesCreateReports(int reportData[30][300], int levelLen, int reportLen
             char line[30];
             fgets(line, sizeof(line), input);
             sscanf(line, "%d %d %d %d %d", &reportData[i][0], &reportData[i][1], &reportData[i][2], &reportData[i][3], &reportData[i][4]);
+            //printf("%d %d %d %d %d\n", reportData[i][0], reportData[i][1], reportData[i][2], reportData[i][3], reportData[i][4]);
         }
     fclose(input);
 }
 
-int checkReports(int * reportBuf, int levelLen, int reportLen)
+int checkReports(int reportBuf[][300], int levelLen, int reportLen)
 {
     int safeCount = 0;
     for (int report = 0; report < reportLen; report++)
         {
-            for (int level = 0; level < levelLen; level++ )
+            int level = 0;
+            if
+                (
+                    reportBuf[report][level] > reportBuf[report][level + 1] &&
+                    reportBuf[report][level + 1] > reportBuf[report][level + 2] &&
+                    reportBuf[report][level + 2] > reportBuf[report][level + 3] &&
+                    reportBuf[report][level + 3] > reportBuf[report][level + 4]
+                )
                 {
-                    // if () // Level is safe
-                    //     {
-                    //         safeCount++:
-                    //     }
+                    safeCount++;
+                }
+            else if
+                (
+                    reportBuf[report][level] < reportBuf[report][level + 1] &&
+                    reportBuf[report][level + 1] < reportBuf[report][level + 2] &&
+                    reportBuf[report][level + 2] < reportBuf[report][level + 3] &&
+                    reportBuf[report][level + 3] < reportBuf[report][level + 4]
+                )
+                {
+                    safeCount++;
+                }
+            else
+                {
+                    safeCount=safeCount;
                 }
         }
     return safeCount;
@@ -36,8 +55,8 @@ int main()
 {
     int safeLevelCount = 0;
     int reportData[30][300];
-    scanFilesCreateReports(reportData, 30, 6);
-    safeLevelCount = checkReports(*reportData, 30, 300);
+    scanFilesCreateReports(reportData, 5, 6);
+    safeLevelCount = checkReports(reportData, 5, 6);
     printf("Safe Level Count: %d\n", safeLevelCount);
     return 0;
 }
