23a44a3f62980221f9f9deb0c453bd09492d607a
Rahul Bhagwat <rahulmbme@gmail.com>
2025-04-24 15:39:26 +0530

    rework

--- TicTacToe/main.c
+++ TicTacToe/main.c
@@ -1,1 +1,35 @@
+# include <stdio.h>
+
+
+void drawGrid (char *selections)
+{
+	for ( int i = 0; i < 15; i++ )
+	{
+		char currentChar = selections [i];
+		if (i % 5 == 0)
+		{
+			printf("\n");
+		}
+		if ( (i % 2) != 0)
+		{
+			printf("|");
+		}
+		else
+		{
+			printf("_");
+		}
+	}
+	return;
+}
+
+int main()
+{
+	char selections[9];
+	for (int i = 0; i <= 8; i++)
+	{
+		selections[i] = '0';
+	}
+	drawGrid(selections);
+	return 0;
+}
 
