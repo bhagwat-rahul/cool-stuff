91bbe862effb21e3e0137fa842a73cf21a2c7127
Rahul Bhagwat <rahulmbme@gmail.com>
2025-04-25 02:45:25 +0530

    input validation bug fixes

--- TicTacToe/main.c
+++ TicTacToe/main.c
@@ -3,8 +3,16 @@
 int makeMoves(char *selections)
 {
     printf("To make a move enter a number from 1-9 to place your symbol\n");
-    int move;
-    scanf("%d", &move);
+    int move, inputValid;
+    inputValid = scanf("%d", &move);
+    while (inputValid != 1 || move < 0 || move > 10 || selections[move - 1] != '0')
+    {
+        if (inputValid != 1) {
+            while (getchar() != '\n');
+        }
+        printf("Invalid move! Remake a move.\n");
+        inputValid = scanf("%d", &move);
+    }
     return move;
 }
 
@@ -12,13 +20,13 @@ char checkWin(char *selections)
 {
     char winningChar = '0';
     if (selections[0] == selections[1] && selections[1] == selections[2]) { winningChar = selections[0]; }
-    if (selections[3] == selections[4] && selections[4] == selections[5]) { winningChar = selections[3]; }
-    if (selections[6] == selections[7] && selections[7] == selections[8]) { winningChar = selections[6]; }
-    if (selections[0] == selections[3] && selections[3] == selections[6]) { winningChar = selections[0]; }
-    if (selections[1] == selections[4] && selections[4] == selections[7]) { winningChar = selections[1]; }
-    if (selections[2] == selections[5] && selections[5] == selections[8]) { winningChar = selections[2]; }
-    if (selections[0] == selections[4] && selections[4] == selections[8]) { winningChar = selections[0]; }
-    if (selections[2] == selections[4] && selections[4] == selections[6]) { winningChar = selections[2]; }
+    else if (selections[3] == selections[4] && selections[4] == selections[5]) { winningChar = selections[3]; }
+    else if (selections[6] == selections[7] && selections[7] == selections[8]) { winningChar = selections[6]; }
+    else if (selections[0] == selections[3] && selections[3] == selections[6]) { winningChar = selections[0]; }
+    else if (selections[1] == selections[4] && selections[4] == selections[7]) { winningChar = selections[1]; }
+    else if (selections[2] == selections[5] && selections[5] == selections[8]) { winningChar = selections[2]; }
+    else if (selections[0] == selections[4] && selections[4] == selections[8]) { winningChar = selections[0]; }
+    else if (selections[2] == selections[4] && selections[4] == selections[6]) { winningChar = selections[2]; }
     return winningChar;
 }
 
@@ -39,7 +47,14 @@ void drawGrid (char *selections)
         {
             if(currentChar == '0')
                 {
-                    printf("_");
+                    if (i >= 12)
+                    {
+                        printf(" ");
+                    }
+                    else
+                    {
+                        printf("_");
+                    }
                 }
             else
                 {
@@ -54,7 +69,8 @@ void drawGrid (char *selections)
                 }
         }
 	}
-	return;
+    printf("\n");
+    return;
 }
 
 int main()
@@ -67,28 +83,25 @@ int main()
 	for (int gameCounter = 0; gameCounter <= 8; gameCounter++)
 	{
 	drawGrid(selections);
-	int moveMade = makeMoves(selections);
+	int moveMade = makeMoves(selections) - 1;
 	if (gameCounter % 2 == 0)
 	{
-	selections[moveMade - 1] = 'X';
+	selections[moveMade] = 'X';
 	}
 	else
 	{
-	selections[moveMade - 1] = 'O';
+	selections[moveMade] = 'O';
 	}
     printf("\n");
     char winCheck = checkWin(selections);
-    if (winCheck == '0')
-        {
-            continue;
-        }
-    else
-        {
-           drawGrid(selections);
-           printf("%c wins!\n", winCheck);
-           return 0;
-        }
+    if (winCheck != '0')
+    {
+        drawGrid(selections);
+        printf("%c wins!\n", winCheck);
+        return 0;
+    }
 	}
+	drawGrid(selections);
 	printf("It's a draw!\n");
 	return 0;
 }
