ea227070838e45aacf2d4ef098ce00ed1fee2606
Rahul Bhagwat <rahulmbme@gmail.com>
2025-04-24 15:57:55 +0530

    grid works?

--- TicTacToe/main.c
+++ TicTacToe/main.c
@@ -3,21 +3,28 @@
 
 void drawGrid (char *selections)
 {
-	for ( int i = 0; i < 15; i++ )
+  for ( int i = 1; i <= 18; i++ )
 	{
-		char currentChar = selections [i];
-		if (i % 5 == 0)
+		char currentChar = selections [i / 2];
+		if (i % 6 == 0)
 		{
-			printf("\n");
+		  printf("\n");
 		}
-		if ( (i % 2) != 0)
-		{
-			printf("|");
-		}
-		else
-		{
-			printf("_");
-		}
+		else if ( (i % 2) == 0)
+        {
+            printf("|");
+        }
+        else
+        {
+            if(currentChar == '0')
+                {
+                    printf("_");
+                }
+            else
+                {
+                    printf("%c", currentChar);
+                }
+        }
 	}
 	return;
 }
@@ -30,6 +37,6 @@ int main()
 		selections[i] = '0';
 	}
 	drawGrid(selections);
+  printf("\n");
 	return 0;
 }
-
