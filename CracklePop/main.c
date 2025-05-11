c2d58f59e16f0185fdca89de43fbd285a7863481
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-11 18:18:52 +0530

    fix indents

--- CracklePop/main.c
+++ CracklePop/main.c
@@ -1,17 +1,15 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    for (int i = 1; i <= 100; i++)
-        {
-            if (i % 3 == 0 && i % 5 == 0)
-                printf("CracklePop\n");
-            else if (i % 3 == 0)
-                printf("Crackle\n");
-            else if (i % 5 == 0)
-                printf("Pop\n");
-            else
-                printf("%d\n", i);
-        }
-    return 0;
+int main() {
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0)
+      printf("CracklePop\n");
+    else if (i % 3 == 0)
+      printf("Crackle\n");
+    else if (i % 5 == 0)
+      printf("Pop\n");
+    else
+      printf("%d\n", i);
+  }
+  return 0;
 }
