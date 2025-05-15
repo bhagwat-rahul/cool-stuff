702ea2a3c9eb01684c4131976b06a642f4aaec7b
Rahul Bhagwat <rahulmbme@gmail.com>
2025-05-15 21:35:03 +0530

    add ai player post 99999 dumb mistakes

--- TicTacToe/main.c
+++ TicTacToe/main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int makeMoves(char *selections) {
-  printf("To make a move enter a number from 1-9 to place your symbol\n");
+int getAIMove(char *selections) {
+  int available, validMoves[9], validMoveCount = 0;
+  for (available = 0; available <= 8; available++) {
+    if (selections[available] == '0') {
+      validMoves[validMoveCount] = available;
+      validMoveCount++;
+    }
+  }
+  int aiMoveIndex = (rand() % validMoveCount) + 1;
+  int aiMove = validMoves[aiMoveIndex];
+  return aiMove;
+}
+
+int makeMoves(char *selections, int multiplayer, int gameCounter) {
   int move, inputValid;
-  inputValid = scanf("%d", &move);
+  if (multiplayer == 0) {
+    printf("To make a move enter a number from 1-9 to place your symbol\n");
+    inputValid = scanf("%d", &move);
+  } else if (multiplayer == 1) {
+    if (gameCounter % 2 == 0) {
+      printf("To make a move enter a number from 1-9 to place your symbol\n");
+      inputValid = scanf("%d", &move);
+    } else {
+      inputValid = 1;
+      move = getAIMove(selections);
+    }
+  } else if (multiplayer == 2) {
+    if (gameCounter % 2 != 0) {
+      printf("To make a move enter a number from 1-9 to place your symbol\n");
+      inputValid = scanf("%d", &move);
+    } else {
+      inputValid = 1;
+      move = getAIMove(selections);
+    }
+  }
   while (inputValid != 1 || move < 1 || move > 9 ||
          selections[move - 1] != '0') {
     if (inputValid != 1) {
@@ -67,12 +99,16 @@ void drawGrid(char *selections) {
 
 int main() {
   char selections[9];
+  int multiplayer = 0;
+  printf(
+      "Do you want to be player 1, 2 or is this 2 humans (0 for 2 humans)!\n");
+  scanf("%d", &multiplayer);
   for (int i = 0; i <= 8; i++) {
     selections[i] = '0';
   }
   for (int gameCounter = 0; gameCounter <= 8; gameCounter++) {
     drawGrid(selections);
-    int moveMade = makeMoves(selections) - 1;
+    int moveMade = makeMoves(selections, multiplayer, gameCounter) - 1;
     if (gameCounter % 2 == 0) {
       selections[moveMade] = 'X';
     } else {
