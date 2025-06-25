f6f6bd6cbd5b9047afa55bfd4b9339621a8708a1
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-25 13:41:43 -0400

    buggy josephus

--- taocp/josephus.py
+++ taocp/josephus.py
@@ -1,17 +1,22 @@
-def josephus (n: int, k: int) -> int:
-	people = list(range(n))
-	while len(people) > 1:
-		for i in range (0,len(people)):
-			if (i % k == 0):
-				people.pop(i)
-				print ("removing ", i)
-				n = n - 1
+# n people are standing in a circle, numbered 0 to n-1.
+# Starting at person 0, every k-th person is eliminated (counting wraps around the circle).
+# The process repeats until only one person remains.
+# Goal: Find the index (0-based) of the survivor.
+
+def josephus () -> int:
+	n:int = int(input("total people?\n"))
+	k:int = int(input("removal offset?\n"))
+	people:list[int] = list(range(0, n))
+	while len(people) != 1:
+		if (k < len(people)):
+			print("TODO: Handle case where k less than num")
+		else:
+			people.pop(k)
+
+	return people[0]
 
 def main ():
-	peoplenum = int(input("how many people?\n"))
-	knum      = int(input("remove at what offset?\n"))
-	josephus(n = peoplenum, k = knum)
+	print ("Result: ", josephus())
 
 if __name__ == "__main__":
     main()
-
