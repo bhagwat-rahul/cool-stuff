dfae240006ec9938032248d2a9e7d9a69002d20c
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-27 23:14:20 -0400

    minor ups

--- Advent-Of-Code-2024/Day-5/solution.cpp
+++ Advent-Of-Code-2024/Day-5/solution.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 void readfile(string& content) {
@@ -15,10 +16,30 @@ void readfile(string& content) {
   file.close();
 }
 
+vector<int> genorder (const vector<pair<int, int>>& pairs)
+{
+	vector<int> res;
+	return res;
+}
+
+vector<pair<int, int>> loadpairs(string)
+{
+	vector<pair<int,int>> pairs;
+	return pairs;
+}
+
+int checkline(vector<pair<int, int>> order, vector<int> line)
+{
+	return 0;
+}
 
 int main() {
 	string content;
   readfile(content);
   cout<<content<<endl;
+  vector<pair<int,int>> pairs = loadpairs(content);
+  // vector<pair<int,int>> vp = {{20,3},{24,8},{12,9},{1,3}};
+  int count = 0;
+  vector<int> sortorder = genorder(pairs);
   return 0;
 }
