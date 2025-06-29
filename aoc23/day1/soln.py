d3aa77e52f2e47536d7f90e2a1ad8e0d898f7354
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-29 17:40:59 -0400

    pt2

--- aoc23/day1/soln.py
+++ aoc23/day1/soln.py
@@ -1,22 +1,31 @@
 def main():
-  count:int = 0
+  count: int = 0
+  nums: dict[str, int] = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+  }
   with open("input.txt", "r") as f:
     document: str = f.read()
-  lines:list[str] = document.splitlines()
+  lines: list[str] = document.splitlines()
   for line in lines:
-    d1:int|None = None; d2:int|None = None;
-    for letter in line:
-      if letter.isdigit():
-        if d1 is not None:
-          d2 = int(letter)
-        else:
-          d1 = int(letter)
-    if d1 is not None and d2 is not None:
-       count+= d1*10+d2
-    if d1 is not None  and  d2 is None:
-       count+= d1*10+d1
-
+    digits: list[int] = []; i:int = 0;
+    for i in range (len(line)):
+      if line[i].isdigit():
+        digits.append(int(line[i]))
+      for word, value in nums.items():
+        if line[i:i + len(word)] == word:
+          digits.append(value)
+    if digits:
+      count += digits[0] * 10 + digits[-1]
   print("result is:", count)
 
 if __name__ == "__main__":
-	main()
+  main()
