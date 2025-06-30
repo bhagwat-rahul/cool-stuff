d9322f76b3243c9d6125bd73ae6d5349093b9d4d
Rahul Bhagwat <rahulmbme@gmail.com>
2025-06-30 13:16:10 -0400

    d3p1 done

--- aoc23/day3/solution.js
+++ aoc23/day3/solution.js
@@ -4,24 +4,58 @@ let lines = input.split("\n");
 lines.pop();
 let count = 0;
 let digitinfo = [];
+let nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 for (let i = 0; i < lines.length; i++) {
   for (let j = 0; j < lines[i].length; j++) {
-    let nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    let valid = 0;
     if (nums.includes(lines[i][j])) {
       let buffer = "";
       while (nums.includes(lines[i][j])) {
         buffer += lines[i][j];
         j++;
       }
-      digitinfo.push({ i: i, j: j - buffer.length, num: buffer, valid: 1 });
+      digitinfo.push({ i: i, j: j - buffer.length, num: buffer, valid: 0 });
     }
   }
 }
+// Function to check bounds and whether a character is a symbol
+function isSymbol(i, j) {
+  if (i < 0 || j < 0 || i >= lines.length || j >= lines[i].length) return false;
+  const ch = lines[i][j];
+  return ch !== "." && !nums.includes(ch);
+}
+
+// Now check validity
 for (let x = 0; x < digitinfo.length; x++) {
-  for (let z = 0; z <= digitinfo[x].num.length; z++) {
-    console.log("checking", lines[digitinfo[x].i][digitinfo[x].j]);
-    // TODO: if any checks fail mark as invalid
+  const { i, j, num } = digitinfo[x];
+  let foundSymbol = false;
+
+  for (let offset = 0; offset < num.length; offset++) {
+    const ci = i;
+    const cj = j + offset;
+
+    const neighbors = [
+      [ci - 1, cj], // top
+      [ci + 1, cj], // bottom
+      [ci, cj - 1], // left
+      [ci, cj + 1], // right
+      [ci - 1, cj - 1], // top-left
+      [ci - 1, cj + 1], // top-right
+      [ci + 1, cj - 1], // bottom-left
+      [ci + 1, cj + 1], // bottom-right
+    ];
+
+    for (const [ni, nj] of neighbors) {
+      if (isSymbol(ni, nj)) {
+        digitinfo[x].valid = 1;
+        foundSymbol = true;
+        break;
+      }
+    }
+    if (foundSymbol) break;
   }
 }
-// console.log(digitinfo);
+
+let validSum = digitinfo
+  .filter((d) => d.valid)
+  .reduce((acc, d) => acc + Number(d.num), 0);
+console.log(validSum);
