6576530b5645be502f1b8ec4ef9849357921f824
Rahul Bhagwat <rahulmbme@gmail.com>
2025-07-22 19:17:57 -0400

    stuff

--- server/main.c
+++ server/main.c
@@ -1,8 +1,13 @@
-// this is a server in c, with verryy little sys deps (sys calls) to be platform independent
+/* this is a server in c, relying on a bunch of abstractions
+   (kernel , driver, tcp ip) to start with. rm 1 by 1 as makes sense. */
 
 # include <stdio.h>
 
 int main()
 {
+    while (1) 
+    {
+        puts("running");
+    }
     return 0;
 }
