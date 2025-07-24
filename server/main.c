6eebb6d9e2dfb22e171e458acbee84629f373c18
Rahul Bhagwat <rahulmbme@gmail.com>
2025-07-24 17:44:57 -0400

    some file parsing + input validation

--- server/main.c
+++ server/main.c
@@ -7,12 +7,81 @@
 # include <netinet/in.h>
 # include <unistd.h>
 # include <string.h> // We can implement this ourselves but let's use for now
+# include <dirent.h>
 
+#define PUBLIC_DIR "./public"
+
+// inspired by althttpd, doing my little bit in the rebellion
 #ifdef HTTP_CRLF
 static const char newline[3] = "\r\n";
 #else
 static const char newline[2] = "\n";
 #endif
+
+static const char allowedInName[] = {
+      /*  x0  x1  x2  x3  x4  x5  x6  x7  x8  x9  xa  xb  xc  xd  xe  xf */
+/* 0x */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* 1x */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* 2x */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+/* 3x */   1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  0,
+/* 4x */   0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+/* 5x */   1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  1,
+/* 6x */   0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
+/* 7x */   1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  1,  0,
+/* 8x */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* 9x */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* Ax */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* Bx */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* Cx */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* Dx */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* Ex */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+/* Fx */   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+};
+
+static int sanitizeString(char *z){
+  int nChange = 0;
+  while( *z ){
+    if( !allowedInName[*(unsigned char*)z] ){
+      char cNew = '_';
+      if( *z=='%' && z[1]!=0 && z[2]!=0 ){
+        int i;
+        if( z[1]=='2' ){
+          if( z[2]=='e' || z[2]=='E' ) cNew = '.';
+          if( z[2]=='f' || z[2]=='F' ) cNew = '/';
+        }
+        for(i=3; (z[i-2] = z[i])!=0; i++){}
+      }
+      *z = cNew;
+      nChange++;
+    }
+    z++;
+  }
+  return nChange;
+}
+
+typedef struct {
+    char *filename;
+    char *content;
+    size_t size;
+} FileEntry;
+
+/* for file in files create a FileEntry for eachfile and return fcount
+ * ret  -(line number) of errored line if errors anywhere
+ */
+int loadfiles()
+{
+  int fcount = 0;
+  DIR *d = opendir(PUBLIC_DIR);
+  if (!d) {
+      perror("opendir");
+      return fcount = -1;
+  }
+  struct dirent *entry;
+  while ((entry = readdir(d))) {
+    fcount++;
+  }
+  return fcount;
+}
 
 int main()
 {
@@ -24,6 +93,13 @@ int main()
   struct sockaddr *addr_ptr = (struct sockaddr *)&addr;
   char *res_code, *res_cont_type, *res_cont_len, *res_cont;
   char headers[512] = "\0", res[2048] = "\0";
+
+  puts("loading webpage files!");
+  int lfile = loadfiles();
+  if (lfile <= 0)
+    {
+      printf("Loading files from %s errored or it is empty\n", PUBLIC_DIR);
+    }
 
   int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (server_fd < 0) {
@@ -43,7 +119,7 @@ int main()
   }
   while (1)
   {
-    char req_buf[1024];
+    char req_buf[1024], method[8], raw_path[256];
     int accepted = accept(server_fd, NULL, NULL);
     if (accepted < 0) {
       puts("failed to accept incoming conn!");
@@ -56,7 +132,8 @@ int main()
         continue;
     }
     req_buf[bytes_read] = '\0';
-    printf("%s", req_buf);
+    printf("Raw req: %s", req_buf);
+    // TODO: Parse path out of req_buf and sanitize safely
 
     res_code      = "HTTP/1.1 200 OK";
     res_cont_type = "Content-Type: text/html";
