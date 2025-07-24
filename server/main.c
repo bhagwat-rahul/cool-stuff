f747a7d37388e558cada69c9f00fcf2a2831f226
Rahul Bhagwat <rahulmbme@gmail.com>
2025-07-24 01:08:46 -0400

    slightly cooler

--- server/main.c
+++ server/main.c
@@ -1,12 +1,18 @@
-/* server relying on a bunch of abstractions
-   (kernel, driver, tcp/ip) to start with.
-   rm 1 by 1 as makes sense. */
+/* single file http(s) server relying on a bunch of
+   abstractions (kernel, driver, tcp/ip) to start with.
+   rm 1 by 1 as makes sense for no deps / baremetal. */
 
 # include <stdio.h> // Debug only
-# include <sys/socket.h>
+# include <sys/socket.h> // unistd usually includes this but better to explicitly include
 # include <netinet/in.h>
 # include <unistd.h>
 # include <string.h> // We can implement this ourselves but let's use for now
+
+#ifdef HTTP_CRLF
+static const char newline[3] = "\r\n";
+#else
+static const char newline[2] = "\n";
+#endif
 
 int main()
 {
@@ -37,32 +43,37 @@ int main()
   }
   while (1)
   {
+    char req_buf[1024];
     int accepted = accept(server_fd, NULL, NULL);
     if (accepted < 0) {
       puts("failed to accept incoming conn!");
       return 1;
     }
-    char buffer[1024];
-    int bytes_read = read(accepted, buffer, sizeof(buffer)-1);
+    int bytes_read = read(accepted, req_buf, sizeof(req_buf)-1);
     if (bytes_read <= 0) {
         puts("read failed or conn closed");
         close(accepted);
         continue;
     }
-    buffer[bytes_read] = '\0';
-    printf("%s", buffer);
+    req_buf[bytes_read] = '\0';
+    printf("%s", req_buf);
 
-    res_code      = "HTTP/1.1 200 OK\n";
-    res_cont_type = "Content-Type: text/html\n";
-    res_cont_len  = "Content-Length: 16\n";
-    res_cont      = "Server healthy!\n";
+    res_code      = "HTTP/1.1 200 OK";
+    res_cont_type = "Content-Type: text/html";
+    res_cont_len  = "Content-Length: 16";
+    res_cont      = "Server healthy!";
 
     strcpy(headers, res_code);
+    strcat(headers, newline);
     strcat(headers, res_cont_type);
+    strcat(headers, newline);
     strcat(headers, res_cont_len);
+    strcat(headers, newline);
+    strcat(headers, newline);
+
     strcat(res, headers);
-    strcat(res, "\n");
     strcat(res, res_cont);
+    strcat(res, newline);
 
     write(accepted, res, strlen(res));
     close(accepted);
