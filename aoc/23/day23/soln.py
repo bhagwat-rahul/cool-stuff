dae86b7a22c7b7b08862356b2f9fdf6a53974186
Rahul Bhagwat <rahulmbme@gmail.com>
2025-07-31 17:33:33 -0400

    mini change

--- aoc/23/day23/soln.py
+++ aoc/23/day23/soln.py
@@ -1,19 +1,11 @@
 def main():
   with open("sample.txt", "r") as f:
     input: str = f.read()
-
   lines:list[str] = input.splitlines()
   steps:int = 0
-  for i in range (len(input)):
-    ...
 
-def getNeighbors(i, j):
-  return [
-    [i - 1, j],      # top
-    [i + 1, j],      # bottom
-    [i, j - 1],      # left
-    [i, j + 1],      # right
-  ]
+def dfs(x:int = 0, y:int = 0, steps:int = 0):
+  ...
 
 if __name__ == "__main__":
   main()
